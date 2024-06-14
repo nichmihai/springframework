3e235a7622ca65c6540ea3f1e7a7b6cdae1e74df
nichm <nichmihai4@gmail.com>
2024-06-13 22:38:42 -0400

    Adding the Bootstrap class

--- src/main/java/nich/mihai/spring6webapp/domain/Book.java
+++ src/main/java/nich/mihai/spring6webapp/domain/Book.java
@@ -2,6 +2,7 @@ package nich.mihai.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -11,13 +12,13 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    public String title;
+    private String title;
     private String isbn;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>();
 
     public Set<Author> getAuthors() {
         return authors;
