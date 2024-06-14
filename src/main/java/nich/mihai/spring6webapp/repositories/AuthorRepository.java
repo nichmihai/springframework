a041b71b10438cec347d5b938434b6f0fca50d58
nichm <nichmihai4@gmail.com>
2024-06-13 22:20:42 -0400

    Adding Book and Author Repository

---
+++ src/main/java/nich/mihai/spring6webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package nich.mihai.spring6webapp.repositories;
+
+import nich.mihai.spring6webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
---
+++ src/main/java/nich/mihai/spring6webapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package nich.mihai.spring6webapp.repositories;
+
+import nich.mihai.spring6webapp.domain.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
