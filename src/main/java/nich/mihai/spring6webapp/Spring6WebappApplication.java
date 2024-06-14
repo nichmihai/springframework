e9af40f252f56670b9fd105212ed12b0ae881500
nichm <nichmihai4@gmail.com>
2024-06-13 22:10:05 -0400

    first commit

---
+++ src/main/java/nich/mihai/spring6webapp/Spring6WebappApplication.java
@@ -0,0 +1,11 @@
+package nich.mihai.spring6webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebappApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6WebappApplication.class, args);
+	}
+}
