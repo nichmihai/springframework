082670cd8bd034b7eaa9af0c589a956c892d1254
nichm <nichmihai4@gmail.com>
2024-06-19 22:19:02 -0400

    Adding Book controller

--- src/main/java/nich/mihai/spring6webapp/controllers/BookController.java
+++ src/main/java/nich/mihai/spring6webapp/controllers/BookController.java
@@ -1,4 +1,25 @@
 package nich.mihai.spring6webapp.controllers;
 
+import nich.mihai.spring6webapp.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookService.findall());
+
+        return "books";
+    }
 }
